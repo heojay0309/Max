2fa47343b2edf94000ff473deacda825539120be
Jay Heo <heojay0309@gmail.com>
2023-09-25 21:11:33 -0700

    max

---
+++ constants/Colors.js
@@ -0,0 +1,30 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+const textColorGray = '#999999';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#F1F1F1',
+    themed: '#fff',
+    QR: '#B2BEB5',
+    tint: tintColorLight,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorLight,
+    gray: textColorGray,
+    connect: '#E3AE09',
+    eid: '#228B22',
+  },
+  dark: {
+    text: '#fff',
+    background: '#0E0E0E',
+    themed: '#000',
+    QR: '#4D414A',
+    tint: tintColorDark,
+    tabIconDefault: '#ccc',
+    tabIconSelected: tintColorDark,
+    gray: textColorGray,
+    connect: '#DCDCDC',
+    eid: '#006766',
+  },
+};
